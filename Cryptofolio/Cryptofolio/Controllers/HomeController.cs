e45c764a2e2d9c0b36e8ef7067a3a0d04917dc46
Mike Szucsik <mszucsik@hotmail.com>
2018-11-30 15:52:02 -0500

    percent fix for chart

--- Cryptofolio/Cryptofolio/Controllers/HomeController.cs
+++ Cryptofolio/Cryptofolio/Controllers/HomeController.cs
@@ -30,6 +30,7 @@ namespace Cryptofolio.Controllers
             DateTime temp = DateTime.Now.AddDays(-33);
             double tempTotalUSD = 0;
             double tempTotalBTC = 0;
+            double tempTotalPurchase = 0;
             double tempPercent = 0;
             double tempBTC = 0;
             List<String> chartDates = new List<String>();
@@ -48,8 +49,12 @@ namespace Cryptofolio.Controllers
                     chartDates.Add(temp.ToShortDateString());
                     chartRatesUSD.Add(tempTotalUSD.ToString());
                     chartRatesBTC.Add(tempTotalBTC.ToString());
+                    tempPercent = tempTotalUSD / tempTotalPurchase * 100;
+                    chartRatesPercent.Add(tempPercent.ToString());
                     tempTotalUSD = 0;
                     tempTotalBTC = 0;
+                    tempTotalPurchase = 0;
+                    tempPercent = 0;
                 }
                 foreach (Holding h in holdings)
                 {
@@ -57,17 +62,22 @@ namespace Cryptofolio.Controllers
                     {
                         tempTotalUSD += h.Amount * m.CurrentPrice;
                         tempTotalBTC += h.Amount * m.CurrentPrice / tempBTC;
+                        tempTotalPurchase += h.Amount * h.PurchasePrice;
                     }
                 }
             }
-
-
+            chartDates.Add(temp.ToShortDateString());
+            chartRatesUSD.Add(tempTotalUSD.ToString());
+            chartRatesBTC.Add(tempTotalBTC.ToString());
+            tempPercent = tempTotalUSD / tempTotalPurchase * 100;
+            chartRatesPercent.Add(tempPercent.ToString());
 
             ViewBag.count = portfolios.Count;
             ViewBag.total = tempTotalUSD;
-            ViewBag.daychange = 20;
+            ViewBag.daychange = tempPercent;
             ViewBag.chartDates = chartDates.Skip(1).ToArray();
             ViewBag.chartRatesUSD = chartRatesUSD.Skip(1).ToArray();
+            ViewBag.chartRatesPercent = chartRatesPercent.Skip(1).ToArray();
             ViewBag.chartRatesBTC = chartRatesBTC.Skip(1).ToArray();
             return View();
         }
@@ -77,93 +87,7 @@ namespace Cryptofolio.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
-
-        public List<Portfolio> getPortfolioStatistics(List<Portfolio> portfolios)
-        {
-            var query = from m in _context.MarketPrice
-                        orderby m.TimeStamp descending
-                        select m;
-
-            List<Asset> assets = _context.Asset.Where(a => a.Activated == true).ToList();
-            List<MarketPrice> marketPrices = query.ToList();
-            List<MarketPrice> displayPrices = new List<MarketPrice>();
-            List<MarketPrice> dayOldPrices = new List<MarketPrice>();
-            var btcPrice = 0.0;
-            foreach (Asset a in assets)
-            {
-                foreach (MarketPrice m in marketPrices)
-                {
-                    if (m.MarketCurrency == "BTC")
-                    {
-                        btcPrice = m.CurrentPrice;
-                    }
-                    if (a.Code == m.MarketCurrency)
-                    {
-                        displayPrices.Add(m);
-                        marketPrices.Remove(m);
-                        break;
-                    }
-                }
-            }
-            foreach (Asset a in assets)
-            {
-                foreach (MarketPrice m in marketPrices)
-                {
-
-                    if (a.Code == m.MarketCurrency)
-                    {
-                        dayOldPrices.Add(m);
-                        break;
-                    }
-                }
-            }
-
-            foreach (Portfolio p in portfolios)
-            {
-                double dailyChange = 0;
-                double totalChange = 0;
-                double percentChange = 0;
-                double totalUSD = 0;
-                double totalBTC = 0;
-                double oldTotal = 0;
-                double totalPurchased = 0;
-
-                List<Holding> holdings = _context.Holding.Where(a => a.Portfolio_ID == p.ID).ToList();
-
-                foreach (Holding h in holdings)
-                {
-                    foreach (MarketPrice m in displayPrices)
-                    {
-                        if (h.AssetType == m.MarketCurrency)
-                        {
-                            totalUSD += h.Amount * m.CurrentPrice;
-                        }
-                    }
-                    foreach (MarketPrice m in dayOldPrices)
-                    {
-                        if (h.AssetType == m.MarketCurrency)
-                        {
-                            oldTotal += h.Amount * m.CurrentPrice;
-                        }
-                    }
-                    totalPurchased += h.PurchasePrice * h.Amount;
-                }
-
-                totalChange = totalUSD - totalPurchased;
-                percentChange = ((totalUSD / totalPurchased) - 1) * 100;
-                dailyChange = (totalUSD - oldTotal) / totalUSD * 100;
-                totalBTC = totalUSD / btcPrice;
-                p.Total_Change = totalChange;
-                p.Daily_Change = dailyChange;
-                p.BTC_Value = oldTotal;
-                p.USD_Value = totalUSD;
-                p.Total_Purchased = totalPurchased;
-                p.Percent_Change = percentChange;
-
-            }
-
-            return portfolios;
-        }
+       
 
     }
 }
